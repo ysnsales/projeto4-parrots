bca767383714f25990438ede6883b26c6c2c3942
Yasmin <ysnsaless@gmail.com>
2023-01-15 16:28:29 -0300

    cartas aleatórias

--- script.js
+++ script.js
@@ -9,43 +9,53 @@ let cartasPossiveis = ['imagens/bobrossparrot.gif', 'imagens/bobrossparrot.gif',
 let cartasEscolhidas = [];
 
 
-function comparador() { 
-	return Math.random() - 0.5; 
-}
-
 function virarcarta(cartaclicada){
+
     cartaclicada.classList.toggle('virar');
 }
 
 let novaCarta;
 let listaCartas;
+let posicao = [];
+let cartasBagunçadas = [];
+
 
 function inicio() {
     let quantidade = Number(prompt('Com quantas cartas deseja jogar?'));
-
+  
 if (quantidade >= 4 && quantidade <= 14 && quantidade % 2 === 0) {
     for (let i=0; i<=(quantidade-1); i++) {
         cartasEscolhidas.push(cartasPossiveis[i]);
         console.log(cartasEscolhidas)
+        
+        posicao.push(i);
+        console.log(posicao);
+    }
+    for (let j = 0; j < quantidade; j++) {
+              let indice = posicao[Math.floor(Math.random()*posicao.length)];
+              console.log(indice);
+              cartasBagunçadas.push(indice)
+              posicao.splice(posicao.indexOf(indice), 1)
 
 
-        novaCarta = `<li class="card" onclick="virarcarta(this)">
+              novaCarta = `<li class="card" onclick="virarcarta(this)">
         <div class="front-face face" >
           <img src="imagens/back.png"/>
         </div>
         <div class="back-face face">
-          <img src=${cartasEscolhidas[i]} />
+          <img src=${cartasEscolhidas[cartasBagunçadas[j]]} />
         </li> `
         
+
         listaCartas  = document.querySelector('.cartas').innerHTML;
         console.log(listaCartas);
         listaCartas = listaCartas + novaCarta;
-
         document.querySelector('.cartas').innerHTML = listaCartas;
+            }
 
-    }
+        }     
 
-}
+
 else {
     alert('Número inválido');
     inicio();
