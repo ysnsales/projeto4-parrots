c2b02ed079f53ab2582cbc4820ef254edd09d4da
Yasmin <ysnsaless@gmail.com>
2023-01-16 14:22:41 -0300

    identificando os pares

--- script.js
+++ script.js
@@ -7,38 +7,89 @@ let cartasPossiveis = ['imagens/bobrossparrot.gif', 'imagens/bobrossparrot.gif',
 'imagens/unicornparrot.gif','imagens/unicornparrot.gif'];
 
 let cartasEscolhidas = [];
+let contador = 0;
 
+
+let click1;
+let click2;
 
 function virarcarta(cartaclicada){
 
     cartaclicada.classList.toggle('virar');
+
+    contador = contador + 1;
+    console.log(contador);
+    if (click1 === undefined){
+    click1 = cartaclicada.innerHTML; 
+    }
+
+    else if (click1 !== undefined && click2 === undefined){
+        click2 = cartaclicada.innerHTML;
+        //cartaclicada.classList.add('disabled');
+
+        if (click1 === click2){
+            mantervirada();
+        }
+        else {
+          setTimeout(desvirar, 1000);
+
+        }
+
+        let site = document.querySelector('.cartas');
+         // site.classList.toggle('disabledSite');
+         // setTimeout(bloqclick, 1000);
+    }
 }
+
+function desvirar(){
+    let colecao = document.getElementsByClassName('virar');
+    let desvira1 = colecao[0];
+    let desvira2 = colecao[1];
+    desvira1.classList.remove('virar');
+    desvira2.classList.remove('virar');
+    click1 = undefined;
+    click2 = undefined;
+    colecao = [];
+}
+
+function mantervirada(){
+    let colecao = document.getElementsByClassName('virar');
+    let vira1 = colecao[0];
+    let vira2 = colecao[1];
+   
+    vira1.classList.remove('virar');
+    vira2.classList.remove('virar');
+    vira1.classList.add('fixar', 'disabled');
+    vira2.classList.add('fixar' , 'disabled');
+    click1 = undefined;
+    click2 = undefined;
+    colecao = [];
+   
+
+    let habilitarclick = document.getElementsByClassName('disabled');
+}
+
 
 let novaCarta;
 let listaCartas;
 let posicao = [];
 let cartasBagunçadas = [];
 
-
 function inicio() {
-    let quantidade = Number(prompt('Com quantas cartas deseja jogar?'));
-  
+    quantidade = Number(prompt('Com quantas cartas deseja jogar?'));
 if (quantidade >= 4 && quantidade <= 14 && quantidade % 2 === 0) {
     for (let i=0; i<=(quantidade-1); i++) {
         cartasEscolhidas.push(cartasPossiveis[i]);
-        console.log(cartasEscolhidas)
         
         posicao.push(i);
-        console.log(posicao);
     }
     for (let j = 0; j < quantidade; j++) {
               let indice = posicao[Math.floor(Math.random()*posicao.length)];
-              console.log(indice);
               cartasBagunçadas.push(indice)
               posicao.splice(posicao.indexOf(indice), 1)
 
 
-              novaCarta = `<li class="card" onclick="virarcarta(this)">
+              novaCarta = `<li class="card" id=${j} onclick="virarcarta(this)">
         <div class="front-face face" >
           <img src="imagens/back.png"/>
         </div>
@@ -48,18 +99,17 @@ if (quantidade >= 4 && quantidade <= 14 && quantidade % 2 === 0) {
         
 
         listaCartas  = document.querySelector('.cartas').innerHTML;
-        console.log(listaCartas);
         listaCartas = listaCartas + novaCarta;
         document.querySelector('.cartas').innerHTML = listaCartas;
             }
 
         }     
 
-
 else {
     alert('Número inválido');
     inicio();
 }
+
 }
 
 inicio();
